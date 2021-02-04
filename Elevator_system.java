a865485f20f94f190515135680fa80276041617d
jerryxiong-code <55557439+jerryxiong-code@users.noreply.github.com>
2021-02-04 15:12:04 -0500

    Update Elevator_system.java

--- Elevator_system.java
+++ Elevator_system.java
@@ -11,74 +11,72 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Elevator_system {
 	public static void main(String[] args) {
 		
-		//Create threads
-		Thread floor_subsystem, elevator, scheduler;
+	//Create threads
+	Thread floor_subsystem, elevator, scheduler;
 
-		scheduler = new Thread(new Scheduler(), "scheduler");
-		floor_subsystem = new Thread(new Floor_subsystem(scheduler),"floor_subsystem");
-		elevator = new Thread(new Elevator(scheduler),"elevator");
+	scheduler = new Thread(new Scheduler(), "scheduler");
+	floor_subsystem = new Thread(new Floor_subsystem(scheduler),"floor_subsystem");
+	elevator = new Thread(new Elevator(scheduler),"elevator");
 
         //Start threads
-		floor_subsystem.start();
-		elevator.start();
-		scheduler.start();
+	floor_subsystem.start();
+	elevator.start();
+	scheduler.start();
 
 	}
 }
 
 class Floor_subsystem implements Runnable{
 	private Thread scheduler;
-    public Floor_subsystem(Thread scheduler)
-    {
-        this.scheduler = scheduler;
-    }
+	public Floor_subsystem(Thread scheduler)
+	{
+        	this.scheduler = scheduler;
+	}
 
-    public void run()
-    {
-        while(true) {
+	public void run()
+	{
+		while(true) {
 
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {}
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {}
         }
     }
 }
 
 class Elevator implements Runnable{
-    private Thread scheduler;
+	private Thread scheduler;
 
-    public Elevator(Thread scheduler)
-    {
-        this.scheduler = scheduler;
-    }
+	public Elevator(Thread scheduler)
+	{
+		this.scheduler = scheduler;
+	}
 
-    public void run()
-    {
-        while(true) {
+	public void run()
+	{
+		while(true) {
 	            
-	        try {
-	            Thread.sleep(1000);
-	        } catch (InterruptedException e) {}
+			try {
+			    Thread.sleep(1000);
+			} catch (InterruptedException e) {}
         	
-        }
-    }
+		}
+	}
 }
 
 class Scheduler implements Runnable{
 
-    public Scheduler()
-    {
+	public Scheduler()
+	{
+		
+	}
 
-    }
-
-    public void run()
-    {
-        while(true) {
-
-
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {}
-        }
-    }
+	public void run()
+	{
+        	while(true) {
+            		try {
+                		Thread.sleep(1000);
+            		} catch (InterruptedException e) {}
+        	}
+	}
 }
