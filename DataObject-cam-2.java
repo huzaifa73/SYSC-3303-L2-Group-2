12bc7a936b328846c88b94d62ad131e742cd8047
Cam Maccoll <cammaccoll@gmail.com>
2021-02-06 18:11:58 -0500

    <Feature> Added FloorSubsystem for interfacing with Elevator and Scheduler classes

---
+++ DataObject-cam-2.java
@@ -0,0 +1,67 @@
+package pack1;
+
+import javax.naming.directory.InvalidAttributesException;
+
+public class DataObject {
+	
+	private String timeString;
+	private boolean upDown;
+	private int targetFloor;
+	private int currentFloor;
+	
+	public DataObject(String timeString, String upDownS, int targetFloor, int currentFloor) throws InvalidAttributesException {
+		this.timeString = timeString;
+		this.targetFloor = targetFloor;
+		this.currentFloor = currentFloor;
+		
+		if(upDownS.toUpperCase().equals("UP"))
+			upDown = true;
+		else if(upDownS.toUpperCase().equals("DOWN"))
+			upDown = false;
+		else {
+			throw new InvalidAttributesException("Direction string not matching UP or DOWN");
+		}
+	}
+	
+	@Override
+	public String toString() {
+		String direction = upDown==true ? "UP" : "DOWN";
+		return new String("TIME: " + timeString + 
+				"\nDIRECTION: " + direction + 
+				"\nTARGET FLOOR: " + targetFloor + 
+				"\nCURRENT FLOOR: " + currentFloor);
+	}
+
+	public String getTimeString(){
+		return this.timeString;
+	}
+	
+	public String getUpDown(){
+		return this.upDown;
+	}
+
+	public String getTargetFloor(){
+		return this.targetFloor;
+	}
+
+	public String getCurrentFloor(){
+		return this.currentFloor;
+	}
+
+	public void setTimeString(timeString){
+		this.timeString = timeString;
+	}
+
+	public void setUpDown(upDown){
+		this.upDown = upDown;
+	}
+
+	public void setTargetFloor(){
+		this.setTargetFloor = targetFloor;
+	}
+
+	
+
+	
+	
+}
