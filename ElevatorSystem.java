a2d05d6b4b94b1a491580920da67fe4965298a5b
jerryxiong-code <55557439+jerryxiong-code@users.noreply.github.com>
2021-02-27 13:18:57 -0500

    Update ElevatorSystem.java

--- ElevatorSystem.java
+++ ElevatorSystem.java
@@ -1,4 +1,5 @@
 package pack;
+import java.io.File;
 import java.util.LinkedList;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -13,23 +14,27 @@ public class ElevatorSystem {
 	public static void main(String[] args) {
 		
 		//Create threads
-		Thread floorSubsystem, elevator, scheduler;
-		Scheduler schedulerObj = new Scheduler();
-		FloorSubsystem floorSubsystemObj = new FloorSubsystem(schedulerObj);
-		Elevator elevatorObj = new Elevator(schedulerObj);
-		schedulerObj.setup(floor_subsystemObj, elevatorObj);
+		Thread floor_subsystem, elevator, scheduler;
 		
+		//Create Objects
+		Scheduler schedulerObj = new Scheduler();
+		
+		//File FloorInputFile.txt should be stored in directly in the project folder
+		File ioFile = new File("FloorInputFile.txt");
+		FloorSubsystem floorSubsystem = new FloorSubsystem(schedulerObj, ioFile);
+		Elevator elevatorObj = new Elevator(schedulerObj);
+		schedulerObj.setup(floorSubsystem, elevatorObj);
+		
+		//Initialize threads
 		scheduler = new Thread(schedulerObj, "scheduler");
-		floorSubsystem = new Thread(new FloorSubsystem(schedulerObj),"floorSubsystem");
+		floor_subsystem = new Thread(floorSubsystem,"floor_subsystem");
 		elevator = new Thread(new Elevator(schedulerObj),"elevator");
 
-		
-		
+
 		//Start threads
-		floorSubsystem.start();
+		floor_subsystem.start();
 		elevator.start();
 		scheduler.start();
 
 	}
 }
-
