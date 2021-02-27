d371ddce4790b4a1a9a600350f6d16062f8e7d4a
jerryxiong-code <55557439+jerryxiong-code@users.noreply.github.com>
2021-02-27 13:51:30 -0500

    Update FloorSubsystem.java
    Add printWrapper

--- FloorSubsystem.java
+++ FloorSubsystem.java
@@ -109,4 +109,16 @@ public class FloorSubsystem implements Runnable{
 			scheduler.receiveRequest(e);
 		}
 	}
+	
+	private void printWrapper(String msg) {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+    		LocalDateTime now = LocalDateTime.now();
+		
+		System.out.println("_____________________________________________________");
+		System.out.println("                Floor Subsystem");
+		System.out.println("-----------------------------------------------------");
+		System.out.println("Log at time: " + dtf.format(now));
+		System.out.println(msg);
+		System.out.println("_____________________________________________________");
+	}
 }
