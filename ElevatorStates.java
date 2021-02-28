13e12ef04e7c90258360d58608f78914f29d031b
jerryxiong-code <55557439+jerryxiong-code@users.noreply.github.com>
2021-02-27 20:25:08 -0500

    Update ElevatorStates.java
    Added comments on the states

--- ElevatorStates.java
+++ ElevatorStates.java
@@ -4,6 +4,23 @@ package pack;
  * Elevator states for state machine
  * @author Blake, Hovish
  *
+ *
+ * State 0: Initial State
+ * There is currently No Task
+ * The elevator is not moving
+ * Requests tasks from scheduler
+ *
+ * State 1:
+ * There is a task
+ * The elevator is currently not on the target floor
+ * Moves to target floor
+ * Checks if a new task takes priority
+ *
+ * State 2:
+ * There is a task
+ * The elevator is on the target floor
+ * Completes the task
+ *
  */
 public enum ElevatorStates {
 	
