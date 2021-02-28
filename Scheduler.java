79017557f647d05a36c8c5588a3bc867b7a70c08
jerryxiong-code <55557439+jerryxiong-code@users.noreply.github.com>
2021-02-27 20:10:06 -0500

    Update Scheduler.java

--- Scheduler.java
+++ Scheduler.java
@@ -1,4 +1,6 @@
 package pack;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.LinkedList;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -19,11 +21,17 @@ class Scheduler implements Runnable{
 	
     //List of completed events (mostly for debug purposes)
     private LinkedList<Event> completedEventList = new LinkedList<>();
-	
+    
+    private schedulerState state;
+    private enum schedulerState {
+    	state0,
+    	state1
+    }
+
     //Constructs the Scheduler
 	public Scheduler()
     {
-
+		state = schedulerState.state0;
     }
     
     //Needs an additional setup method
@@ -34,14 +42,46 @@ class Scheduler implements Runnable{
     
     //receive_request from the floor system or elevators
     public synchronized void receiveRequest(Event event) {
-    	eventList.add(event);
+    	//eventList.add(event);
+    	
+    	//loop through list
+    	int curr = elevator.getCurrentFloor();
+    	//if event's target floor is in the opposite direction, put it at the end (sort it later)
+    	//TODO sort it later means when completing the last task of a particular direct, we flip the direction and resort the list
+    	
+	//Check if the event floor is on the way. Then check if the direction is correct
+    	if(elevator.getUpDown() == (curr <= event.getTargetFloor()) || (event.getUpDown() == (event.getTargetFloor() <= elevator.getTargetFloor()))) { //TODO check what this means...
+
+        	//if the target floor is in the right direction:
+        	//put it in the order of closest to current floor of that elevator
+        	for (int i = 0; i < eventList.size(); i++) {
+        		if((event.getTargetFloor() - curr) < (eventList.get(i).getTargetFloor() - curr)) {
+        			eventList.add(i, event);
+        		}
+        	}
+    	}else {
+    		//opposite direction
+    		printWrapper("Received request in the opposite direction");
+    		eventList.add(event);
+    	}
+    	
+    	//TODO check that event was added.
+    	setState();
     	System.out.println("New event added: " + eventList.get(eventList.size()-1));
     	notifyAll();
     }
     
+    private void setState() {
+    	if(eventList.size() == 0) {
+    		state = schedulerState.state0;
+    	}else {
+    		state = schedulerState.state1;
+    	}
+    }
+    
     //Allows elevator to request an event
     public synchronized void requestEvent() {
-    	if(eventList.size() != 0) {
+    	if(state == schedulerState.state1) {
     		elevator.receiveRequest(eventList.peekFirst());
     	}
     	notifyAll();
@@ -58,6 +98,7 @@ class Scheduler implements Runnable{
     	completedEventList.add(event);
     	eventList.remove(event);
     	sendData(event);
+    	setState();
 
     }
     
@@ -83,17 +124,18 @@ class Scheduler implements Runnable{
             } catch (InterruptedException e) {}
         }
     }
-	
-	
-    private void printWrapper(String msg) {
-	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+    
+    
+    
+	private void printWrapper(String msg) {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
     	LocalDateTime now = LocalDateTime.now();
 		
-	System.out.println("_____________________________________________________");
-	System.out.println("                 Scheduler");
-	System.out.println("-----------------------------------------------------");
-	System.out.println("Log at time: " + dtf.format(now));
-	System.out.println(msg);
-	System.out.println("_____________________________________________________");
-    }
+		System.out.println("_____________________________________________________");
+		System.out.println("                 Scheduler");
+		System.out.println("-----------------------------------------------------");
+		System.out.println("Log at time: " + dtf.format(now));
+		System.out.println(msg);
+		System.out.println("_____________________________________________________");
+	}
 }
