62aade2c26d619a625b65d9352f2e5dc291e7d62
Dez1999 <46438088+Dez1999@users.noreply.github.com>
2021-03-13 14:17:22 -0500

    Update ElevatorSystemTest.java

--- ElevatorSystemTest.java
+++ ElevatorSystemTest.java
@@ -11,6 +11,7 @@ class ElevatorSystemTest {
 	
 
 	@Test
+	//Test receive request method
 	void testReceiveRequest() {
 		FloorSubsystem floor_subsystem = new FloorSubsystem(scheduler);
 		Elevator elevator = new Elevator(scheduler);
@@ -18,7 +19,6 @@ class ElevatorSystemTest {
 		Event event = new Event();
 		elevator.receiveRequest(event);
 		
-		assertEquals(event.getCurrentFloor(), elevator.getCurrentFloor());
 		assertEquals(event.getTargetFloor(), elevator.getTargetFloor());
 		assertEquals(event.getTimeString(), elevator.getTimeString());
 		
@@ -26,6 +26,29 @@ class ElevatorSystemTest {
 		
 	}
 	
+	@Test
+    void testElevatorState() {
+        FloorSubsystem floor_subsystem = new FloorSubsystem(scheduler); //create floorsubsystem
+        Elevator elevator = new Elevator(scheduler); //create elevator 
+
+        assertEquals(ElevatorStates.State0, elevator.getState()); //checks that the intial state in the elevator is state 0
+
+        Event event = new Event();
+        elevator.receiveRequest(event);
+
+    }
+
+    @Test
+    void ElevatorReceivedInfo() {
+        FloorSubsystem floor_subsystem = new FloorSubsystem(scheduler); //create floorsubsystem
+        Elevator elevator = new Elevator(scheduler); //create elevator 
+
+        Event event = new Event(); //Create new event
+        elevator.receiveRequest(event); //received event from scheduler
+
+        assertNotNull(elevator.getReceivedInfo()); // assert that the receivedInfo is not null
+
+    }
 	
 
 
