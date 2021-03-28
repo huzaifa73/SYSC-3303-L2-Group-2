dc50a83e3b19b9a1ec273a5ccdeaab6864911542
Dez1999 <46438088+Dez1999@users.noreply.github.com>
2021-03-27 23:09:43 -0400

    Update ElevatorSystemTest.java

--- ElevatorSystemTest.java
+++ ElevatorSystemTest.java
@@ -2,34 +2,36 @@ package pack;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.naming.directory.InvalidAttributesException;
+
 import org.junit.jupiter.api.Test;
 
 class ElevatorSystemTest {
-	Scheduler scheduler = new Scheduler();
-	Elevator elevator = new Elevator(scheduler, 0);
-	
-	
+		
 
 	@Test
 	//Test receive request method
 	void testReceiveRequest() {
-		FloorSubsystem floor_subsystem = new FloorSubsystem(scheduler);
-		Elevator elevator = new Elevator(scheduler, 1);
+		Elevator elevator = new Elevator(1);
 		
-		Event event = new Event();
-		//elevator.receiveRequest(event);
-		
-		assertEquals(event.getTargetFloor(), elevator.getTargetFloor());
-		assertEquals(event.getTimeString(), elevator.getTimeString());
-		
+		try {
+			Event event = new Event(false, 0, "Down", 4, 2, 0);
+			
+			elevator.readInfo(event);
+			
+			assertEquals(event.getTargetFloor(), elevator.getTargetFloor());
+			assertEquals(event.getTimeString(), elevator.getTimeString());
+		} catch (InvalidAttributesException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 	}
 	
 	@Test
     void testElevatorState() {
-        FloorSubsystem floor_subsystem = new FloorSubsystem(scheduler); //create floorsubsystem
-        Elevator elevator = new Elevator(scheduler, 0); //create elevator 
+        Elevator elevator = new Elevator(0); //create elevator 
 
         assertEquals(ElevatorStates.idleState, elevator.getState()); //checks that the initial state in the elevator is state 0
 
@@ -41,11 +43,10 @@ class ElevatorSystemTest {
 
     @Test
     void ElevatorReceivedInfo() {
-        FloorSubsystem floor_subsystem = new FloorSubsystem(scheduler); //create floorsubsystem
-        Elevator elevator = new Elevator(scheduler, 1); //create elevator 
+        Elevator elevator = new Elevator(1); //create elevator 
 
         Event event = new Event(); //Create new event
-        //elevator.receiveRequest(event); //received event from scheduler
+        elevator.readInfo(event); //received event from scheduler
 
         assertNotNull(elevator.getReceivedInfo()); // assert that the receivedInfo is not null
 
