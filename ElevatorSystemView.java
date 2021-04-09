47e89ffdbf59d24ac89989d53429cc9c9c42bc98
Dez1999 <46438088+Dez1999@users.noreply.github.com>
2021-04-08 21:17:14 -0400

    Create ElevatorSystemView.java

---
+++ ElevatorSystemView.java
@@ -0,0 +1,10 @@
+/**
+ * Elevator System Interface View Class
+ * @version: April 8, 2021
+ */
+package pack;
+
+public interface ElevatorSystemView {
+	void handleElevatorSystemUpdate(ElevatorSystemEvent e) throws InterruptedException;
+
+}
